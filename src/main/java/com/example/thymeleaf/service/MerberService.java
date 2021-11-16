fec21985b558c3d87a8de24104884a5447ea98b8
tmaxaipsJCL <chaelyn_jang@tmax.co.kr>
2021-11-16 15:14:58 +0900

    service 구현

---
+++ src/main/java/com/example/thymeleaf/service/MerberService.java
@@ -0,0 +1,37 @@
+package com.example.thymeleaf.service;
+
+import com.example.thymeleaf.domain.Member;
+import com.example.thymeleaf.repository.MemberRepository;
+import com.example.thymeleaf.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MerberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member){
+
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 이름 안받도록 하는 비즈니스 로직
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
